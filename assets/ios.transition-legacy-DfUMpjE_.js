b9812ccfb58547839353461d7b31e6cb9c653dc4
ast4rt3 <lawrenze3206@gmail.com>
2025-02-11 23:19:14 +0800

    Updates

--- assets/ios.transition-legacy-DfUMpjE_.js
+++ assets/ios.transition-legacy-DfUMpjE_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C6wSw6e6.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-SfUQ-Gx0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
