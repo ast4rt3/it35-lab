eb496f47f9e941d1cd76f307dbf0beb691b385b1
ast4rt3 <lawrenze3206@gmail.com>
2025-02-11 23:39:12 +0800

    Updates

--- assets/ios.transition-legacy-BiWK7yzu.js
+++ assets/ios.transition-legacy-BiWK7yzu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-SfUQ-Gx0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-U-ulTfc8.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-T_eCutem.js
+++ assets/keyboard2-legacy-T_eCutem.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-SfUQ-Gx0.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-U-ulTfc8.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
