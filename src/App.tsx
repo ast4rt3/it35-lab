e27427205d274e1599c8eb2bc6ccab24eef8499c
ast4rt3 <lawrenze3206@gmail.com>
2025-02-11 23:12:16 +0800

    Added login, home,template and sidemenu

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,15 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import Login from './pages/Login'; // ✅ Ensure Login component exists
 
-/* Core CSS required for Ionic components to work properly */
+/* Core CSS */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
-import './theme/variables.css';
 
 setupIonicReact();
 
@@ -41,6 +20,9 @@ const App: React.FC = () => (
       <IonRouterOutlet>
         <Route exact path="/it35-lab/home">
           <Home />
+        </Route>
+        <Route exact path="/it35-lab/login">
+          <Login />
         </Route>
         <Route exact path="/it35-lab/">
           <Redirect to="/it35-lab/home" />
---
+++ src/components/Sidemenu.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+
+const Sidemenu: React.FC = () => {
+  const history = useHistory();
+
+  return (
+    <IonMenu side="start" contentId="main-content">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonItem button onClick={() => history.push('/home')}>Home</IonItem>
+          <IonItem button onClick={() => history.push('/details')}>Details</IonItem>
+          <IonItem button onClick={() => history.push('/login')}>Logout</IonItem>
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default Sidemenu;
