b52145912116d1365ae4079a958dc00ad162a3b9
ast4rt3 <lawrenze3206@gmail.com>
2025-02-14 14:26:04 +0800

    Updates

--- assets/keyboard2-legacy-B_9nBAlR.js
+++ assets/keyboard2-legacy-B_9nBAlR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8Pw8zJgd.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BBp4EwV3.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-uyIM6mz7.js
+++ assets/status-tap-legacy-uyIM6mz7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8Pw8zJgd.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-BBp4EwV3.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
