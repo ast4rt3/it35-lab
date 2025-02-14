0fec3e15b5438bd42932b76dcf55942215022a64
Law <[lawrenze3206@gmail.com]>
2025-02-14 09:04:48 +0800

    Added sidemenu template part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Menu from './pages/Menu';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -33,6 +33,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 import Login from './pages/Login';
+import Menu from './pages/Menu';
 
 setupIonicReact();
 
@@ -41,7 +42,7 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route exact path="/it35-lab" component={Login} />
-        <Route exact path="/it35-lab/app" component={Menu} />
+        <Route exact path="/it35-lab" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
