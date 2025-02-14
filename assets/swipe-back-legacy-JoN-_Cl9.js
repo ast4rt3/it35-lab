eab7b9ef6c1b811c543e79ec4e16511ec28c7b5e
ast4rt3 <lawrenze3206@gmail.com>
2025-02-14 18:44:43 +0800

    Updates

--- assets/swipe-back-legacy-JoN-_Cl9.js
+++ assets/swipe-back-legacy-JoN-_Cl9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DjcoKPYo.js","./index3-legacy-DxLahuNQ.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-u8nKrBYW.js","./index3-legacy-B_77FAuW.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
