0dd1eb4f00b573480813f8b85fd1f1023c676c4e
ast4rt3 <lawrenze3206@gmail.com>
2025-02-14 14:11:18 +0800

    Added logout button on menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,5 @@
 import { 
-  IonButtons,
+    IonButton,
     IonContent, 
     IonHeader, 
     IonIcon, 
@@ -13,7 +13,7 @@ import {
     IonTitle, 
     IonToolbar 
 } from '@ionic/react';
-  import {homeOutline, rocketOutline} from 'ionicons/icons';
+  import {homeOutline, logOutOutline, rocketOutline} from 'ionicons/icons';
   import { Redirect, Route } from 'react-router';
   import Home from './Home';
   import About from './About';
@@ -43,6 +43,12 @@ const Menu: React.FC = () => {
                                 </IonItem>
                             </IonMenuToggle>
                         ))}
+
+                         {/*Logout Button*/}
+                         <IonButton routerLink="/it35-lab" routerDirection="back" expand="full">
+                            <IonIcon icon={logOutOutline} slot="start"> </IonIcon>
+                        Logout
+                         </IonButton>
                     </IonContent>
                 </IonMenu>
                 
