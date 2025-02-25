60b0cd87a0d708c6ddca59a6f262ab0e38431b8e
ast4rt3 <lawrenze3206@gmail.com>
2025-02-25 22:09:19 +0800

    Small fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   base: "/it35-lab/", 
@@ -13,5 +14,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude],
   }
 });
