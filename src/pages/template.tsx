7c6b5ba11132524a7c43784e36e2abad40296ac9
ast4rt3 <lawrenze3206@gmail.com>
2025-02-25 21:53:08 +0800

    Merge from login template to main

--- src/pages/template.tsx
+++ src/pages/template.tsx
@@ -1,28 +1,19 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Template: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Template</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Template;
+import React from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+
+const Template: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Template Page</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <h2>Welcome to the Template Page</h2>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Template;
