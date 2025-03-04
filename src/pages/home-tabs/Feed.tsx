941b4ba73221a3b90ccca1525c7033782f3a3ea6
ast4rt3 <lawrenze3206@gmail.com>
2025-03-04 23:04:16 +0800

    Added content on home feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,38 +1,65 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Feed: React.FC = () => {
-    return (
+import {
+  IonButtons,
+  IonCard,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+
+
+const Feed: React.FC = () => {
+
+
+
+  return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Main</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
+          <IonHeader>
+              <IonToolbar>
+                  <IonButtons slot='start'>
+                      <IonMenuButton></IonMenuButton>
+                  </IonButtons>
+                  <IonTitle>Home</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+          <IonContent fullscreen>
+          <div className="dashboard-container">
+        
+          <IonCard className="overview">
+            <h2>Overview</h2>
+            
+            <div className="chart-placeholder">[Chart Placeholder]</div>
+          </IonCard>
+
+          {/* Placeholder Chart */}
+          <IonCard className="placeholder-chart">
+            <h2>Add Subtitle Here</h2>
+            <div className="chart-placeholder">[Placeholder]</div>
+          </IonCard>
+
+          {/* Alerts Section */}
+          <IonCard className="alerts">
+            <h2>Alerts</h2>
+            <button className="add-alert">+</button>
+            <div className="alert-placeholder">[Alert List]</div>
+          </IonCard>
+
+          {/* Events Section */}
+          <IonCard className="events">
+            <h2>Events</h2>
+            
+          </IonCard>
         </div>
-        </IonContent>
+          </IonContent>
       </IonPage>
-    );
-  };
-  
-  export default Feed;
+  );
+};
+
+export default Feed;
