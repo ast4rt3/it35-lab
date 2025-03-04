556670807f74f2ad9612a60c4cbd2a3810560730
ast4rt3 <lawrenze3206@gmail.com>
2025-03-04 20:09:17 +0800

    Updates

--- assets/swipe-back-legacy-wYwcKGi7.js
+++ assets/swipe-back-legacy-wYwcKGi7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CjyAcF-V.js","./index3-legacy-BJbxJiZo.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-RL7GWYxN.js","./index3-legacy-izbV_vZO.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
