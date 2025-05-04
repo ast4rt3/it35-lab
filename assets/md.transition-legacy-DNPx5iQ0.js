98ed094510a7d29cc157853226e108cc3837e122
ast4rt3 <lawrenze3206@gmail.com>
2025-05-04 20:28:20 +0800

    Updates

--- assets/md.transition-legacy-DNPx5iQ0.js
+++ assets/md.transition-legacy-DNPx5iQ0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ced8zjzV.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-COIBbsT0.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
