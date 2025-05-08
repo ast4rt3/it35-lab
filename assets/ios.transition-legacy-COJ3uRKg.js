8a126806437804f5ffcc80940cde23751fc1041b
ast4rt3 <lawrenze3206@gmail.com>
2025-05-09 03:12:30 +0800

    Updates

--- assets/ios.transition-legacy-COJ3uRKg.js
+++ assets/ios.transition-legacy-COJ3uRKg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-COIBbsT0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-3YLHzRp5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.t,n=t.u}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
