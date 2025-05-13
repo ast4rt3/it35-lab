8d78ac679f8ec2f6cd4fb26aa0acbe45f2afd4ab
ast4rt3 <lawrenze3206@gmail.com>
2025-05-14 00:09:51 +0800

    Updates

--- assets/browser-w31hGAZ1.js
+++
@@ -1,2 +0,0 @@
-import{v as o}from"./index-BGUQm5lU.js";var r,e;function t(){return e||(e=1,r=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}),r}var s=t();const n=o(s),w=Object.freeze(Object.defineProperty({__proto__:null,default:n},Symbol.toStringTag,{value:"Module"}));export{w as b};
-//# sourceMappingURL=browser-w31hGAZ1.js.map
