4474cd7d6fcaf20d02aa95618bbc16e1f30d3d58
ast4rt3 <lawrenze3206@gmail.com>
2025-05-13 22:11:39 +0800

    Cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,11 @@
 import { IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 import { bookOutline, star } from 'ionicons/icons';
 import { Route } from 'react-router';
-
-import Favorites from './home-tabs/Favorites';
 import Feed from './home-tabs/Feed';
 
 const Home: React.FC = () => {
   const tabs = [
     { name: 'Feed', tab: 'feed', url: '/it35-lab/app/home/feed', icon: bookOutline },
-    { name: 'Favorites', tab: 'favorites', url: '/it35-lab/app/home/favorites', icon: star },
   ];
 
   return (
@@ -16,20 +13,12 @@ const Home: React.FC = () => {
       <IonRouterOutlet>
         {/* Define routes for feed and favorites tabs */}
         <Route exact path="/it35-lab/app/home/feed" component={Feed} />
-        <Route exact path="/it35-lab/app/home/favorites" component={Favorites} />
 
         {/* Default route for /home to show Feed component */}
         <Route exact path="/it35-lab/app/home" component={Feed} />
       </IonRouterOutlet>
 
-      <IonTabBar slot="bottom">
-        {tabs.map((item, index) => (
-          <IonTabButton key={index} tab={item.tab} href={item.url}>
-            <IonIcon icon={item.icon} />
-            <IonLabel>{item.name}</IonLabel>
-          </IonTabButton>
-        ))}
-      </IonTabBar>
+   
     </IonTabs>
   );
 };
